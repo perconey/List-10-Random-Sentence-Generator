67c4d47a8adc0e70657188aa3f494a496ab7b897
Perconey <perconey@gmail.com>
2017-11-13 22:22:30 +0100

    Setup repo

---
+++ Sentence Generator/Sentence Generator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentence_Generator
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Are you sure abpur recreating whole language-o-algorythmic process?
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
